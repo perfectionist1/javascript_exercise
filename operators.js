6595f198fcbf17e08c31ace8fe3fa06cd9421a4a
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2020-06-24 16:43:02 +0600

    Javascript Operators

---
+++ operators.js
@@ -0,0 +1,47 @@
+/*
+::::::::: Arithmetic Operators::::::::::::::
++ 	Addition
+- 	Subtraction
+* 	Multiplication
+** 	Exponentiation (ES2016)
+/ 	Division
+% 	Modulus (Division Remainder)
+++ 	Increment
+-- 	Decrement
+
+::::::::::: Assignment Operators ::::::::
+= 	x = y 	x = y
++= 	x += y 	x = x + y
+-= 	x -= y 	x = x - y
+*= 	x *= y 	x = x * y
+/= 	x /= y 	x = x / y
+%= 	x %= y 	x = x % y
+**= 	x **= y
+
+:::::::::::::: Comparison Operators ::::::::::
+== 	equal to
+=== equal value and equal type
+!= 	not equal
+!== not equal value or not equal type
+> 	greater than
+< 	less than
+>= 	greater than or equal to
+<= 	less than or equal to
+? 	ternary operator
+
+:::::::::::::: Logical Operators ::::::::
+&& 	logical and
+|| 	logical or
+! 	logical not
+
+::::::::::::::::::::Bitwise Operators :::::::::::
+& 	AND 	5 & 1 	0101 & 0001 	0001 	 1
+| 	OR 		5 | 1 	0101 | 0001 	0101 	 5
+~ 	NOT 	~ 5 	 ~0101 	1010 	 10
+^ 	XOR 	5 ^ 1 	0101 ^ 0001 	0100 	 4
+<< 	Zero 	fill left shift 	5 << 1 	0101 << 1 	1010 	 10
+>> 	Signed 	right shift 	5 >> 1 	0101 >> 1 	0010 	  2
+>>> 		Zero fill right shift
+
+
+*/
